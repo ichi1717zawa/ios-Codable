36811e9d149e409aa339641f38b187e665b35e22
skyockqq38@gmail.com <62639010+yi-sudo@users.noreply.github.com>
2020-07-03 02:48:45 +0800

    home不要再動了！！！！！

--- ShareBox/Controller/allPostVC.swift
+++ ShareBox/Controller/allPostVC.swift
@@ -519,20 +519,23 @@ class allPostVC: UIViewController,UITableViewDelegate,UITableViewDataSource, UIS
     }
  
     func pressCategoryButton(button:UIButton,categoryName:String){
+        
         UIView.animate(withDuration: 0.3) {
             self.initButton()
-            
-                 self.categoryControllButtenView.frame.origin.x = super.view.frame.origin.x + 10
+                     self.categoryControllButtenView.frame.origin.x = super.view.frame.origin.x + 10
+                
                  self.searchButton.alpha = 1
+           
             button.setImage(UIImage(named: categoryName), for: .normal)
 //                 button.alpha = 0.6
+            
              }
     }
  
     @IBOutlet weak var categoryControllButtenView: UIStackView!
     @IBAction func btn1(_ sender: Any) {
-       
-//       self.jio.frame.origin.x = super.view.frame.origin.x
+
+ 
         selectCategoryLabel.text = "ALL" 
         pressCategoryButton(button:btn1, categoryName: "ALL彩")
         
@@ -597,16 +600,16 @@ class allPostVC: UIViewController,UITableViewDelegate,UITableViewDataSource, UIS
 //            btn8: btn8.setImage(UIImage(named: "ALL灰"),
 //            btn9: btn9.setImage(UIImage(named: "ALL灰"),
 //            btn10: btn10.setImage(UIImage(named: "ALL灰")
-            btn1.setImage(UIImage(named: "ALL灰"), for: .normal);btn1.alpha = 1
-            btn2.setImage(UIImage(named: "書籍文具票券灰"), for: .normal);btn2.alpha = 1
-            btn3.setImage(UIImage(named: "3c產品灰"), for: .normal);btn3.alpha = 1
-            btn4.setImage(UIImage(named: "玩具電玩灰"), for: .normal);btn4.alpha = 1
-            btn5.setImage(UIImage(named: "生活居家與家電灰"), for: .normal);btn5.alpha = 1
-            btn6.setImage(UIImage(named: "影視音娛樂灰"), for: .normal);btn6.alpha = 1
-            btn7.setImage(UIImage(named: "飲品食品灰"), for: .normal);btn7.alpha = 1
-            btn8.setImage(UIImage(named: "保養彩妝灰"), for: .normal);btn8.alpha = 1
-            btn9.setImage(UIImage(named: "男裝配件灰"), for: .normal);btn9.alpha = 1
-            btn10.setImage(UIImage(named: "女裝婦幼灰"), for: .normal);btn10.alpha = 1
+            btn1.setImage(UIImage(named: "ALL灰"), for: .normal)
+            btn2.setImage(UIImage(named: "書籍文具票券灰"), for: .normal)
+            btn3.setImage(UIImage(named: "3c產品灰"), for: .normal)
+            btn4.setImage(UIImage(named: "玩具電玩灰"), for: .normal)
+            btn5.setImage(UIImage(named: "生活居家與家電灰"), for: .normal)
+            btn6.setImage(UIImage(named: "影視音娛樂灰"), for: .normal)
+            btn7.setImage(UIImage(named: "飲品食品灰"), for: .normal)
+            btn8.setImage(UIImage(named: "保養彩妝灰"), for: .normal)
+            btn9.setImage(UIImage(named: "男裝配件灰"), for: .normal)
+            btn10.setImage(UIImage(named: "女裝婦幼灰"), for: .normal)
  
     }
     
@@ -619,6 +622,15 @@ class allPostVC: UIViewController,UITableViewDelegate,UITableViewDataSource, UIS
         }
     }
     
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        if textField.text?.count != 0 {
+            searchImage.alpha = 0
+        }else{
+            searchImage.alpha = 1
+        }
+    }
+    
+    @IBOutlet weak var searchImage: UIImageView!
     
 }
  
